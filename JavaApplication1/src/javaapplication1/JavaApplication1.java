cbea049776413b65e9f1271ca7bde50c33f61fde
Zohaib73-pixel <siddiq73@sheridancollege.ca>
2025-06-16 14:58:33 -0400

    one last

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,17 @@
+package studentlist;
+* @author siddiq73
+public class Student
+//model student details
+private String name ; //principle encapsulation
+private int sid;
+//getter/setter
+/**
+*,
+* @return the fame
+public String getName () {
+return name;
+}
+/**
+*/
+* @param name the name to set
+public void setName (String name) {
